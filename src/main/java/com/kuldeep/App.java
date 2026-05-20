c280dfa3ab88e659cc4d94e40b661737725b8b68
Kuldeep Sharma <kuldeepsharma78607860@gmail.com>
2026-05-20 23:30:49 +0530

    nre

--- src/main/java/com/kuldeep/App.java
+++ src/main/java/com/kuldeep/App.java
@@ -1,11 +1,7 @@
 package com.kuldeep;
 
-/**
- * Hello world!
- */
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello Jenaakinsssttts + Mavenworking again with ci/cd again
-");
+        System.out.println("Hello Kuldeep with jenkis");
     }
 }
