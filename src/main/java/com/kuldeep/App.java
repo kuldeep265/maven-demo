8b0cf52b9c750d58209f1dee8331d504fb9fbe4f
Kuldeep Sharma <kuldeepsharma78607860@gmail.com>
2026-05-20 22:02:24 +0530

    webhook test

--- src/main/java/com/kuldeep/App.java
+++ src/main/java/com/kuldeep/App.java
@@ -5,6 +5,6 @@ package com.kuldeep;
  */
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello Jenkins + Maven");
+        System.out.println("Hello Jenkins + Mavenworking with ci/cd");
     }
 }
